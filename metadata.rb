b9d496bb72beed4311d70bff1fd51521feb0a1ce
krowpu <krowpu@tightmail.com>
2017-03-19 15:24:09 +0500

    Add metadata

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,12 @@ name 'redis'
 description 'Redis server'
 license 'MIT'
 
+maintainer 'krowpu'
+maintainer_email 'krowpu@tightmail.com'
+
+source_url 'https://github.com/krowpu/chef-redis'
+issues_url 'https://github.com/krowpu/chef-redis/issues'
+
 platform 'ubuntu', '= 16.04'
 
 depends 'dist-update'
